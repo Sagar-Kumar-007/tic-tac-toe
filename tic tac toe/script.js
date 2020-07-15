04fe32be37d5db6ea0b28ce21ec26caa23445822
Sagar-Kumar-007 <sk8814594@gmail.com>
2020-07-15 10:27:13 +0530

    my first project

---
+++ tic tac toe/script.js
@@ -0,0 +1,61 @@
+const cellElements=document.querySelectorAll("[data-cell]");
+const board=document.getElementById("board");
+const x_class="x";
+const o_class="o";
+var j=0;
+var a=new Array();
+for(var i=1;i<=9;i++)
+{
+    a[i]=i;
+}
+var circleturn=false;
+cellElements.forEach(cell=>cell.addEventListener("click",handleclick,{once:true}));
+function handleclick(e){
+    j++;
+    var cell=e.target;
+    var currentclass;
+    if(circleturn){
+        currentclass=o_class;
+        //hovering effect
+        board.className=x_class;
+    }
+    else{
+        currentclass=x_class;
+        //hovering effect
+        board.className=o_class;
+    }
+    this.className+=currentclass;
+    
+    //Win or draw
+    if(circleturn){
+        a[cell.id]=o_class;
+    }
+    else{
+        a[cell.id]=x_class;
+    }
+   // console.log(cell.id);
+   // console.log(typeof cell.id);
+   //console.log(a[1]);
+   //console.log(a[2]);
+   //console.log(a[3]);
+    if(((a[1]===a[2] && a[2]===a[3]) || (a[4]===a[5] && a[5]===a[6]) || (a[7]===a[8] && a[8]===a[9]) || (a[1]===a[4] && a[4]===a[7]) || (a[2]===a[5] && a[5]===a[8]) || (a[3]===a[6] && a[6]===a[9]) || (a[1]===a[5] && a[5]===a[9]) || (a[3]===a[5] && a[5]===a[7]))){
+        console.log(currentclass +" win");
+        document.querySelector(".data").innerHTML="<h1>"+currentclass.toUpperCase()+" Wins</h1>";
+        document.querySelector("#message").className+="show";
+    }
+    else if(j==9 && !(((a[1]===a[2] && a[2]===a[3]) || (a[4]===a[5] && a[5]===a[6]) || (a[7]===a[8] && a[8]===a[9]) || (a[1]===a[4] && a[4]===a[7]) || (a[2]===a[5] && a[5]===a[8]) || (a[3]===a[6] && a[6]===a[9]) || (a[1]===a[5] && a[5]===a[9]) || (a[3]===a[5] && a[5]===a[7])))){
+        console.log("Draw");
+        document.querySelector(".data").innerHTML="<h1>Draw!</h1>";
+        document.querySelector("#message").className+="show";
+    }
+    console.log(j);
+    //Swapping chance
+    circleturn=!circleturn;
+};
+function restart(){
+//console.log("restart")
+//document.querySelector("#board").className="x";
+//document.querySelectorAll(".cell").forEach(cell=>cell.className="cell ");
+//document.querySelector("#message").className="msg ";
+location.reload();
+};
